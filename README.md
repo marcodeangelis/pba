f47ca0c0eacee95f7d9d9101d661fafd481b9b3c
Marco <marco.deangelis09@gmail.com>
2021-04-15 11:52:48 +0100

    first commit

--- README.md
+++ README.md
@@ -5,3 +5,77 @@ High-level algorithms for probability bounds analysis
 Cite W&D 
 
 Cite Ferson
+
+
+## Example: Cantilever beam
+
+```python
+unitconversionfactor = 1
+def pba(steps=200) :
+    y = pbox(normal(0.15, 0.0021))
+    L = pbox(interval(9.95,10.05))
+    I = pbox(normal(5e-4, 2.6e-5))
+    F = pbox(weibull(5.17, 27.5)) 
+    E = pbox(normal(200, 3.4), normal(220, 3.4))
+    # assuming independence
+    Id = unitconversionfactor * Idiv( Imul(F,L^3), Imul(3*E,I))
+    Iσ = unitconversionfactor * Idiv( Imul(F, Imul(L,y)), I)
+    # Frechet (no assumption about dependence)
+    Fd = unitconversionfactor * Fdiv( Fmul(F,L^3), Fmul(3*E,I))
+    Fσ = unitconversionfactor * Fdiv( Fmul(F, Fmul(L,y)), I)
+    return Id,Iσ,Fd,Fσ
+```
+
+
+### Import the necessary library
+#### External libraries
+
+```python
+import numpy
+import scipy.stats as scipystats
+from numpy import mean, std
+```
+
+#### Import Frechet
+```python
+from frechet import pbox, interval, plot
+```
+
+### Get handle to continuous distribution object
+```python 
+normal = scipystats.norm
+weibull = scipystats.weibull_min
+uniform = scipystats.uniform
+```
+
+### Instantiate the p-boxes using the frechet module with 200 steps
+
+```python
+steps=200
+y  = pbox(*[normal(0.15, 0.0021)]*2,steps=steps)
+L3 = pbox(*interval(9.95**3,10.05**3), unbounded=False, interval=True,steps=steps)
+L = pbox(*interval(9.95,10.05), unbounded=False, interval=True,steps=steps)
+I  = pbox(*[normal(5e-4, 2.6e-5)]*2,steps=steps)
+F  = pbox(*[weibull(5.17, 27.5)]*2,steps=steps) 
+E3 = pbox(*[normal(3*200, 3*3.4), normal(3*220, 3*3.4)],steps=steps)
+```
+
+### Compute the Frechet bounds with *conversion factor = 1*
+
+```pyhton
+Id =  frechet_div( frechet_times(F,L3), frechet_times(E3,I) )
+Iσ =  frechet_div( frechet_times( F, frechet_times(L,y) ), I )
+```
+
+### Plot the results
+```python
+plot(Id,xlabel='displacement')
+```
+![png](img/output_4_0.png)
+
+```python
+plot(Id,xlabel='stress')
+```
+
+![png](img/output_5_0.png)
+
---
+++ frechet.py
@@ -0,0 +1,140 @@
+"""
+Created on Wed Apr 15 2021
+@author: Marco De Angelis
+University of Liverpool
+github.com/marcodeangelis
+MIT License
+
+# Code the computation of Frechet bounds with intrusive arithmetic.
+
+To see how this code can be used in practice, check the Readme.md file in this repository. 
+
+
+
+"""
+
+
+from matplotlib import pyplot
+import numpy
+
+
+def interval(lo,hi):
+    return [lo, hi]
+
+def pbox(leftcdf,rightcdf,steps=50, unbounded=True, interval=False):
+    a_l = leftcdf
+    a_r = rightcdf
+    p = numpy.linspace(0.001,0.999,steps+1)
+    if interval:
+        p_l = list(p).copy()
+        p_r = list(p).copy()
+        x_l = [a_l] * (steps+1)
+        x_r = [a_r] * (steps+1)
+    else:
+        p_r = list(p).copy()
+        p_l = list(p[1:])+list([p[-1]])
+        x_l = list(a_l.ppf(p))
+        x_r = list(a_r.ppf(p))
+    
+    l = [x_l,p_l]
+    r = [x_r,p_r]
+    return l,r
+    
+def plot(X,ax=None):
+    def stepdata(x,y): # x,y must be python lists
+        xx,yy = x*2, y*2
+        xx.sort()
+        yy.sort()
+        return xx, [0.]+yy[:-1]
+    if ax is None:
+        fig,ax = pyplot.subplots(figsize=(12,9))
+    x,y=stepdata(X[0][0],X[0][1])
+    ax.plot(x,y)
+    x,y=stepdata(X[1][0],X[1][1])
+    ax.plot(x,y)
+
+
+
+# ----- Arithmetic operations start here ----- #
+
+# X[0][0]: x-values left cdf bound 
+# X[0][1]: p-values left cdf bound 
+
+# X[1][0]: x-values right cdf bound 
+# X[1][1]: p-values right cdf bound 
+
+def frechet_sum(X,Y):
+    p_Z_hi = X[0][1]
+    p_Z_lo = X[1][1]
+    X_lo, Y_lo = X[1][0][:-1], Y[1][0][:-1]
+    X_hi, Y_hi = X[0][0][1:],  Y[0][0][1:]
+    N = len(X_lo)
+    Z_lo, Z_hi = [], []
+    for i in range(N):
+        z_lo = z_hi = []
+        for j in range(i,N):
+            z_lo.append(X_lo[j]+Y_lo[i-j+N-1])
+        for j in range(i):
+            z_hi.append(X_hi[j]+Y_lo[i-j])
+        Z_lo.append(min(z_lo))
+        Z_hi.append(max(z_hi))
+
+    Z = (([Z_hi[0]]+Z_hi,p_Z_hi),(Z_lo+[Z_lo[-1]],p_Z_lo))
+    return Z
+
+def frechet_minus(X,Y):
+    p_Z_hi = X[0][1]
+    p_Z_lo = X[1][1]
+    X_lo, Y_lo = X[1][0][:-1], Y[1][0][:-1]
+    X_hi, Y_hi = X[0][0][1:],  Y[0][0][1:]
+    N = len(X_lo)
+    Z_lo, Z_hi = [], []
+    for i in range(N):
+        z_lo = z_hi = []
+        for j in range(i,N):
+            z_lo.append(X_lo[j]-Y_lo[j-i])
+        for j in range(i):
+            z_hi.append(X_hi[j]-Y_lo[j-i+N-1])
+        Z_lo.append(min(z_lo))
+        Z_hi.append(max(z_hi))
+
+    Z = (([Z_hi[0]]+Z_hi,p_Z_hi),(Z_lo+[Z_lo[-1]],p_Z_lo))
+    return Z
+
+def frechet_times(X,Y):
+    p_Z_hi = X[0][1]
+    p_Z_lo = X[1][1]
+    X_lo, Y_lo = X[1][0][:-1], Y[1][0][:-1]
+    X_hi, Y_hi = X[0][0][1:],  Y[0][0][1:]
+    N = len(X_lo)
+    Z_lo, Z_hi = [], []
+    for i in range(N):
+        z_lo = z_hi = []
+        for j in range(i,N):
+            z_lo.append(X_lo[j]*Y_lo[i-j+N-1])
+        for j in range(i):
+            z_hi.append(X_hi[j]*Y_lo[i-j])
+        Z_lo.append(min(z_lo))
+        Z_hi.append(max(z_hi))
+
+    Z = (([Z_hi[0]]+Z_hi,p_Z_hi),(Z_lo+[Z_lo[-1]],p_Z_lo))
+    return Z
+
+def frechet_div(X,Y):
+    p_Z_hi = X[0][1]
+    p_Z_lo = X[1][1]
+    X_lo, Y_lo = X[1][0][:-1], Y[1][0][:-1]
+    X_hi, Y_hi = X[0][0][1:],  Y[0][0][1:]
+    N = len(X_lo)
+    Z_lo, Z_hi = [], []
+    for i in range(N):
+        z_lo = z_hi = []
+        for j in range(i,N):
+            z_lo.append(X_lo[j]/Y_lo[j-i])
+        for j in range(i):
+            z_hi.append(X_hi[j]/Y_lo[j-i+N-1])
+        Z_lo.append(min(z_lo))
+        Z_hi.append(max(z_hi))
+
+    Z = (([Z_hi[0]]+Z_hi,p_Z_hi),(Z_lo+[Z_lo[-1]],p_Z_lo))
+    return Z
